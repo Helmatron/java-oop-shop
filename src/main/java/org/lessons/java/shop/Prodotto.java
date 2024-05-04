1651543c084128198b3517c215d06997d10d346d
Helmatron <158586047+Helmatron@users.noreply.github.com>
2024-05-04 12:34:58 +0200

    Start: assegnazione valori Prodotto

---
+++ src/main/java/org/lessons/java/shop/Prodotto.java
@@ -0,0 +1,15 @@
+package org.lessons.java.shop;
+
+public class Prodotto {
+
+	private int code;
+	private String name;
+	private String description;
+	private double price;
+	private static int IVA = 22;
+	
+	public Prodotto() {
+		
+	}
+	
+}
